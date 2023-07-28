6c7a5d7c7e727dcc7f07d44e494aec02f103b7e3
psyjc21 <psyjc21@nottingham.ac.uk>
2023-07-28 23:07:36 +0100

    Created a function to convert infix to postfix
    Postfix will make it a lot easier to evaluate maths expressions.

--- script.js
+++ script.js
@@ -1,26 +1,71 @@
-let textScreen = document.querySelector(".screen .text");
-const buttons = document.querySelectorAll("button");
-let text = textScreen.textContent;
-console.log(text);
+// let textScreen = document.querySelector(".screen .text");
+// const buttons = document.querySelectorAll("button");
+// let text = textScreen.textContent;
+// console.log(text);
 
-buttons.forEach((button) =>{
-    button.addEventListener('click', btnClick);
-});
+// buttons.forEach((button) =>{
+//     button.addEventListener('click', btnClick);
+// });
 
-function btnClick(event){
-    let n = event.target.textContent;
-    if(n == "AC"){
-        textScreen.innerHTML = "";
-        return;
-    }
-    else if(n == "."){
-        if(textScreen.textContent[textScreen.textContent.length-1] == "."){
-            return;
-        }
-    }
-    else if(n == "="){
+// function btnClick(event){
+//     let n = event.target.textContent;
+//     if(n == "AC"){
+//         textScreen.innerHTML = "";
+//         return;
+//     }
+//     else if(n == "."){
+//         if(textScreen.textContent[textScreen.textContent.length-1] == "."){
+//             return;
+//         }
+//     }
+//     else if(n == "="){
         
+//     }
+//     textScreen.textContent += n; 
+// }
+
+
+  function precedence(operator) {
+    if (operator === '+' || operator === '-') return 1;
+    if (operator === '*' || operator === '/') return 2;
+    return 0;
+  }
+  
+  function shuntingYard(expression) {
+    const outputQueue = [];
+    const operatorStack = [];
+  
+    const tokens = expression.split(/\s+/);
+  
+    tokens.forEach((token) => {
+      if (!isNaN(parseFloat(token))) {
+        outputQueue.push(parseFloat(token));
+      } else if (token === '+' || token === '-' || token === '*' || token === '/') {
+        while (
+          operatorStack.length > 0 &&
+          precedence(token) <= precedence(operatorStack[operatorStack.length - 1])
+        ) {
+          outputQueue.push(operatorStack.pop());
+        }
+        operatorStack.push(token);
+      } else if (token === '(') {
+        operatorStack.push(token);
+      } else if (token === ')') {
+        while (operatorStack.length > 0 && operatorStack[operatorStack.length - 1] !== '(') {
+          outputQueue.push(operatorStack.pop());
+        }
+        operatorStack.pop(); // Discard the left bracket
+      }
+    });
+  
+    while (operatorStack.length > 0) {
+      outputQueue.push(operatorStack.pop());
     }
-    textScreen.textContent += n;
-    
-}
+  
+    return outputQueue;
+  }
+  
+  // Test the function
+  const expression = "3 + 4 * ( 2 - 1 ) / 5";
+  const result = shuntingYard(expression);
+  console.log(result); // Output: [3, 4, 2, 1, '-', '*', 5, '/', '+']
