8db20f81690fc813f15a197d55ba17a825d206ac
psyjc21 <psyjc21@nottingham.ac.uk>
2023-07-28 21:32:24 +0100

    added functional keypad
    allows users to type into screen and clear it

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 let textScreen = document.querySelector(".screen .text");
 const buttons = document.querySelectorAll("button");
-console.log(buttons);
+let text = textScreen.textContent;
+console.log(text);
+
+buttons.forEach((button) =>{
+    button.addEventListener('click', btnClick);
+});
+
+function btnClick(event){
+    let n = event.target.textContent;
+    if(n == " AC "){
+        textScreen.innerHTML = "";
+        return;
+    }
+    textScreen.textContent += n;
+    
+    
+}
